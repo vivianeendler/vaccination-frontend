a761860668ea1846b041cd72563343d0b655bb46
vivianeendler <vdendler@inf.ufsm.br>
2022-04-15 15:31:31 -0300

    form validation schema

---
+++ src/components/ScheduleForm/validationSchema.js
@@ -0,0 +1,16 @@
+import * as Yup from "yup";
+
+const schema = Yup.object({
+    name: Yup.string().required("O campo Nome é de preenchimento obrigatório."),
+    email: Yup.string().email("Favor digitar um e-mail válido."),
+    dateTimeOption: Yup.date()
+        .required(
+            "O campo de seleção de Dia e Horário para agendamento é de preenchimento obrigatório.",
+        )
+        .min(new Date(), "Favor selecionar uma data válida."),
+    bornDate: Yup.date()
+        .required("O campo Nascimento é de preenchimento obrigatório.")
+        .max(new Date(), "Favor selecionar uma data válida."),
+});
+
+export default schema;

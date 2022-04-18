f518f1aae9f50024d2e029f873bc6ef3f419efdb
vivianeendler <vdendler@inf.ufsm.br>
2022-04-18 11:58:31 -0300

    update query with api data

--- src/Pages/Query/QueryModal.js
+++ src/Pages/Query/QueryModal.js
@@ -1,25 +1,45 @@
 import Table from "../../components/Table";
 import DatePicker from "react-datepicker";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { setHours, setMinutes } from "date-fns";
-const rows = [
-    {
-        nome: "Viviane",
-        nascimento: "17/03/1997",
-        email: "viviane@viviane",
-        horario: "17/03/22 14:30",
-    },
-];
+import { Pencil } from "tabler-icons-react";
+import OpenModal from "./OpenModal";
+import OrderTable from "../../utils/OrderTable";
+
+import axios from "../../services/api";
 
 const QueryModal = () => {
     const [value, setValue] = useState(setHours(setMinutes(new Date(), 0), 8));
+    const [rows, setRows] = useState([]);
+    const [modal, setModal] = useState({ opened: false, data: null });
+    const [row, setRow] = useState([null]);
+
+    const onClick = (row) => {
+        setModal({ opened: !modal.opened });
+        setRow(row);
+    };
+
+    const actions = [
+        {
+            leftIcon: <Pencil />,
+            onClick: (row) => onClick(row),
+        },
+    ];
+    const toggle = () => {
+        setModal({ ...modal, opened: !modal.opened });
+    };
+
+    useEffect(() => {
+        axios.get("/schedules").then((response) => setRows(response.data));
+    }, []);
+    rows.sort(OrderTable);
     return (
         <>
             <h1>Consultar agendamentos</h1>
-            <label htmlFor="dateTimeOption">Selecionar</label>
+            <label htmlFor="scheduledTo">Selecionar</label>
             <DatePicker
-                id="dateTimeOption"
-                name="dateTimeOption"
+                id="scheduledTo"
+                name="scheduledTo"
                 selected={value}
                 onChange={(date) => setValue(date)}
                 showTimeSelect
@@ -31,13 +51,19 @@ const QueryModal = () => {
 
             <Table
                 columns={[
-                    { key: "horario", value: "Horário" },
-                    { key: "nome", value: "Nome" },
-                    { key: "nascimento", value: "Nascimento" },
+                    { key: "scheduledTo", value: "Horário" },
+                    { key: "name", value: "Nome" },
+                    { key: "bornDate", value: "Nascimento" },
                     { key: "email", value: "Email" },
+                    { key: "status", value: "Status" },
+                    { key: "observation", value: "Conclusão" },
                 ]}
                 rows={rows}
+                actions={actions}
             />
+            {modal.opened ? (
+                <OpenModal opened={modal} toggle={toggle} schedule={row} />
+            ) : null}
         </>
     );
 };
